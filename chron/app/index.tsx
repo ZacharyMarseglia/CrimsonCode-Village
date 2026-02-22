a026afbc5f95196ca8fa1725572731968b1fa0fa
Evan Glasscock <evan.glasscock@wsu.edu>
2026-02-21 22:00:48 -0800

    Create ActionButton component

--- chron/app/index.tsx
+++ chron/app/index.tsx
@@ -1,5 +1,12 @@
+import ActionButton from "@/components/ui/action-button";
+import { Pressable } from "react-native";
+
 export default function HomePage() {
     return (
-        <div>Hello!</div>
+        <div>
+            <div>Hello!</div>
+
+            <ActionButton title="Press me" onPress={() => alert('Pressed!')} />
+        </div>
     );
 }
